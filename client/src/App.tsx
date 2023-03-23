7c290a239fca33ae735973567dfa0a09aaf3f431
Sava Stanchev <s.stanchev94@gmail.com>
2023-03-23 13:34:42 +0200

    added a navbar and a flex component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { useMemo } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./scenes/navbar";
 import { themeSettings } from "./theme";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
             <Routes>
               <Route path="/" element={<div>dashboard page</div>} />
               <Route
