b22665a0ea091c37f312a20c6f1abdc7a06d107d
Sava Stanchev <s.stanchev94@gmail.com>
2023-03-25 11:23:57 +0200

    add types for kpi response & format data

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
+import KPI from "./models/KPI.js";
+import { kpis } from "./data/data.js";
 
 // CONFIGURATIONS
 dotenv.config();
@@ -33,5 +35,9 @@ mongoose
   })
   .then(async () => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    // ADD DATA ONE TIME ONLY OR AS NEEDED
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis);
   })
   .catch((error) => console.log(`${error} did not connect`));
